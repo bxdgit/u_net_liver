a02de69f1957462ec2c36a2670d202b8c665c883
bianxindong <bxd_best@163.com>
2021-09-24 17:27:39 +0800

    训练参数

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from numpy.core.fromnumeric import size
 import torch
 import argparse
 from torch.utils.data import DataLoader
@@ -12,14 +13,19 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 x_transforms = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+    transforms.Resize((1024,1024)),
+    transforms.Normalize([0.5], [0.5])
 ])
 
 # mask只需要转换为tensor
-y_transforms = transforms.ToTensor()
+y_transforms = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Resize((1024,1024)),
+])
 
 def train_model(model, criterion, optimizer, dataload, num_epochs=20):
     for epoch in range(num_epochs):
+        model.train()
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
         dt_size = len(dataload.dataset)
@@ -44,19 +50,19 @@ def train_model(model, criterion, optimizer, dataload, num_epochs=20):
 
 #训练模型
 def train(args):
-    model = Unet(3, 1).to(device)
+    model = Unet(1, 1).to(device)
     batch_size = args.batch_size
     criterion = nn.BCEWithLogitsLoss()
-    optimizer = optim.Adam(model.parameters())
-    liver_dataset = LiverDataset("data/train",transform=x_transforms,target_transform=y_transforms)
+    optimizer = optim.Adam(model.parameters(),1e-3)
+    liver_dataset = LiverDataset("/home/xindong/project/Unet-pytorch/data/imagesTr",transform=x_transforms,target_transform=y_transforms)
     dataloaders = DataLoader(liver_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
     train_model(model, criterion, optimizer, dataloaders)
 
 #显示模型的输出结果
 def test(args):
-    model = Unet(3, 1)
+    model = Unet(1, 1)
     model.load_state_dict(torch.load(args.ckpt,map_location='cpu'))
-    liver_dataset = LiverDataset("data/val", transform=x_transforms,target_transform=y_transforms)
+    liver_dataset = LiverDataset("/home/xindong/project/Unet-pytorch/data/imagesTs", transform=x_transforms,target_transform=y_transforms)
     dataloaders = DataLoader(liver_dataset, batch_size=1)
     model.eval()
     import matplotlib.pyplot as plt
@@ -75,7 +81,7 @@ if __name__ == '__main__':
     parse=argparse.ArgumentParser()
     parse = argparse.ArgumentParser()
     parse.add_argument("action", type=str, help="train or test")
-    parse.add_argument("--batch_size", type=int, default=8)
+    parse.add_argument("--batch_size", type=int, default=2)
     parse.add_argument("--ckpt", type=str, help="the path of model weight file")
     args = parse.parse_args()
 
